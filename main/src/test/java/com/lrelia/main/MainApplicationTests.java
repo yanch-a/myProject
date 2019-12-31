6254260a040425061a5f5d87f64b514067089380
yanch-a <807777263@qq.com>
2019-12-31 18:00:45 +0800

    初始提交

---
+++ main/src/test/java/com/lrelia/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lrelia.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
